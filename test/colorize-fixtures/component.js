8443ac7befa3e6eac0d6fefa9e60a9d94a14f7c3
Jason Quense <monastic.panic@gmail.com>
2019-09-11 15:58:40 -0400

    feat: more css-modules stuff

--- test/colorize-fixtures/component.js
+++ test/colorize-fixtures/component.js
@@ -1,7 +1,12 @@
 const Component = styled(Component)`
   @value col: #foo;
 
+  :export {
+
+  }
+
   composes: ${styles.boo} from './bar.scss';
+  composes: ${styles.boo} from global;
 
   color: #ebebeb;
 `
