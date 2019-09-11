5fea298fa76583ee7c78c4454c036586e33daf97
Jason Quense <monastic.panic@gmail.com>
2019-09-11 16:18:15 -0400

    fix: composes class names

--- test/colorize-fixtures/component.js
+++ test/colorize-fixtures/component.js
@@ -1,7 +1,7 @@
 const Component = styled(Component)`
   @value col: #foo;
 
-  composes: fsf fsfs ${styles.boo} from './bar.scss';
+  composes: fsf-2 fsfs ${styles.boo} from './bar.scss';
   composes: ${styles.boo} from global;
 
   color: #ebebeb;
